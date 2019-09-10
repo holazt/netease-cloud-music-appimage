2745605f059a092fbe782849ec8a81756939de64
Ahmed <10769951+holazt@users.noreply.github.com>
2019-09-10 20:00:04 +0800

    chore: change upload service

--- scripts/appimage_after_success.sh
+++ scripts/appimage_after_success.sh
@@ -4,4 +4,6 @@ set -e
 
 cd ${ROOT_PATH}/scripts/services
 
-TEMP_DOWNLOAD_URL=$(python3 transferwee.py upload NetEase_Cloud_Music-1.2.0.2.*.AppImage)
+# TEMP_DOWNLOAD_URL=$(python3 transferwee.py upload NetEase_Cloud_Music-1.2.0.2.*.AppImage)
+
+TEMP_DOWNLOAD_URL=$(bash ${ROOT_PATH}/scripts/services/transfer.sh.sh NetEase_Cloud_Music-1.2.0.2.*.AppImage)
